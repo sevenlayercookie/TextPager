6462fdc268c88ec5e77caf0657201727dbaa07f2
sevenlayercookie <sevenlayercookie@gmail.com>
2022-10-22 02:15:56 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+
+function openPagerWindow() {
+    x = document.getElementById('newinput').value
+    window.open("http://javaprod.mayo.edu/ADR/MainController?pageid=240&S=TOD&name=Custom%20Text%20Page&pagernumber=127%20"+x+"&addr_owner=MC/SMC");
+}
